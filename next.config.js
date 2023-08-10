42fa8391ab0b4ba6f400064c0b08a3f3383d4a05
NGUYỄN VĂN THUẦN <59735665+thuannv201@users.noreply.github.com>
2023-08-10 02:40:53 +0000

    ucii

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    basePath: "/thuannv-github-pages",
+    images: {
+        unoptimized: true,
+    },
+}
 
 module.exports = nextConfig
